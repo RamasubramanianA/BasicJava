11fd3d54666df5e09a3681d3d8c9de87c49fca16
ram <ram24subbu4@gmail.com>
2017-04-30 18:36:11 +0530

    first

---
+++ ObjIns.java
@@ -0,0 +1,18 @@
+package firstproject.com.basicJava;
+ class name {
+	name (){
+		System.out.println("ram");			
+	}
+}
+public class ObjIns {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		name n1 = new name();
+	}
+	
+	
+
+}
